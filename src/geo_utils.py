aafbbc587cda717c129a97c0da54ebfd595c4b1d
armkhudinyan <armkhudinyan@gmail.com>
2024-05-22 18:10:04 +0100

    Fixes bug in exception message catchong

--- src/geo_utils.py
+++ src/geo_utils.py
@@ -1,9 +1,12 @@
+import logging as log
 import geopandas as gpd
 from pathlib import Path
 from shapely.wkt import loads
 
-from .exceptions import WKTError
-
+log.basicConfig(
+    level = log.INFO,
+    # filename = local_config.log_path,
+    format = "%(levelname)s: %(message)s")
 
 def is_wkt(text: str) -> bool:
     """Check if the input string is in WKT format."""
@@ -26,10 +29,14 @@ def to_openeo_wkt(aoi: Path | str | None) -> str | None:
                 gdf = gpd.read_file(aoi)
                 return gdf.unary_union.wkt
             except Exception as e:
-                raise WKTError(e)
+                log.info(f"{e.__class__.__name__}: {e} \n"
+                         "AOI will not be considered for this query")
+                return None
 
     try:
         gdf = gpd.read_file(aoi)
         return gdf.unary_union.wkt
     except Exception as e:
-        raise WKTError(e)
+        log.info(f"{e.__class__.__name__}: {e} \n"
+                 "AOI will not be considered for this query")
+        return None
